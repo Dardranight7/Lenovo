ea0fa3610346d1bae29ccecee8dbdafa89159423
Dardranight7 <dardranight@gmail.com>
2025-08-29 18:43:39 -0500

    Update Hearth

--- GoatHeart/Assets/GoatHeart/Scripts/MultiDisplaySetup.cs
+++ GoatHeart/Assets/GoatHeart/Scripts/MultiDisplaySetup.cs
@@ -13,14 +13,14 @@ public class MultiDisplaySetup : MonoBehaviour
         // --- Display principal (0) ---
         // Lo ponemos en vertical (1080 x 1920) en modo ventana
         Screen.fullScreenMode = FullScreenMode.Windowed;
-        Screen.SetResolution(1080, 1920, false);
+        Screen.SetResolution(1920, 1080, false);
 
         // --- Display secundario (1) ---
         if (Display.displays.Length > 1)
         {
             // SetParams(width, height, x, y)
             // Renderiza a 1920x1080 en modo ventana
-            Display.displays[1].SetParams(1920, 1080, 0, 0);
+            Display.displays[1].SetParams(1080, 1920, 0, 0);
         }
 
         Debug.Log("✅ Displays configurados en modo ventana (puedes moverlos manualmente).");
