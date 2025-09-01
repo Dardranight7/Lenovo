5e6f44ac28acc64de1912db09c54f6a348b18a77
Dardranight7 <dardranight@gmail.com>
2025-09-01 16:21:26 -0500

    Update VideoDownloader.cs

--- BodyLenovo/Assets/_Body/Scripts/VideoDownloader.cs
+++ BodyLenovo/Assets/_Body/Scripts/VideoDownloader.cs
@@ -1,16 +1,49 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.IO;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Networking;
 
 public class VideoDownloader : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public string videoURL;
+    public string videoPath;
+    public string videoName;
+    public UnityEvent OnVideoDownloaded;
+
+    private void Start()
     {
-        
+        int index = PlayerPrefs.GetInt("videoIndex" + videoName, 0);
+        videoPath = Path.Combine(Application.persistentDataPath, videoName + index + ".mp4");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DownloadVideo()
     {
-        
+        StartCoroutine(DownloadAndStore());
+    }
+
+    private IEnumerator DownloadAndStore()
+    {
+        // 1. Descargar archivos
+        yield return DownloadFile(videoURL, videoPath);
+    }
+
+    private IEnumerator DownloadFile(string url, string savePath)
+    {
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        www.downloadHandler = new DownloadHandlerFile(savePath);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            UnityEngine.Debug.LogError("❌ Error descargando " + url + " -> " + www.error);
+        }
+        else
+        {
+            UnityEngine.Debug.Log("✅ Archivo descargado en: " + savePath);
+            OnVideoDownloaded?.Invoke();
+            PlayerPrefs.SetInt("videoIndex" + videoName, PlayerPrefs.GetInt("videoIndex" + videoName, 0) + 1);
+        }
     }
 }
